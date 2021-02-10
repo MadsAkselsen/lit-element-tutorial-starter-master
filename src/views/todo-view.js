50ebbd33ac2f22045b1dc4273c77531b73d48950
Mads Akselsen <akselsenmads@gmail.com>
2021-02-10 19:15:26 +0100

    created todo-view component

---
+++ src/views/todo-view.js
@@ -0,0 +1,9 @@
+const { LitElement } = require('lit-element');
+
+class TodoView extends LitElement {
+  render() {
+    return html` <p>Hello World</p> `;
+  }
+}
+
+customElements.define('todo-view', TodoView);
